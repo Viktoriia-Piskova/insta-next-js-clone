df2a6e2c6385df31b191559f613df0c96b47e34b
piskova <v.piskova@beetroot.se>
2024-04-10 14:12:42 +0300

    Base react modal and react icons setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 "use client"
 
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import Modal from 'react-modal';
+import { IoMdAddCircleOutline } from 'react-icons/io';
 
 export default function Header() {
     const { data: session } = useSession();
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className='shadow-sm border-b sticky top-0 bg-white z-30 p-3'>
             <div className='flex justify-between items-center max-w-6xl mx-auto'>
@@ -27,11 +30,27 @@ export default function Header() {
 
                 {/* menu items */}
                 {session ?
-                    (<img src={session.user.image} alt={session.user.name} className='h-10 w-10 rounded-full cursor-pointer shadow block' onClick={signOut} />
+                    (<div className='flex gap-2 items-center'>
+                        <IoMdAddCircleOutline
+                            className='text-2xl cursor-pointer tranform hover:scale-125 transition duration-300 hover:text-red-600'
+                            onClick={() => setIsOpen(true)}
+                        />
+                        <img src={session.user.image} alt={session.user.name} className='h-10 w-10 rounded-full cursor-pointer shadow block' onClick={signOut} />
+
+                    </div>
                     ) : (
                         <button onClick={() => signIn()} className='text-sm font-semibold text-blue-500'>Log in</button>
                     )}
             </div>
+
+            {isOpen && (
+                <Modal isOpen={isOpen} className='max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white shadow-md border-2 rounded-md' onRequestClose={() => setIsOpen(false)} ariaHideApp = {false}>
+                    <div>
+                        <h1>Modal</h1>
+                        <button onClick={() => setIsOpen(false)}>Close</button>
+                    </div>
+                </ Modal>
+            )}
         </div>
-    )
+    );
 }
