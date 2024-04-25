9dce6eb80d8985921d1c34ffcf979e29630fde45
piskova <v.piskova@beetroot.se>
2024-04-25 18:27:53 +0300

    Complete like functionality

--- src/components/LikeSection.jsx
+++ src/components/LikeSection.jsx
@@ -1,21 +1,21 @@
 "use client";
 
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getFirestore,
+  onSnapshot,
+  setDoc,
+} from "firebase/firestore";
 import React, { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { HiOutlineHeart, HiHeart } from "react-icons/hi";
 import { app } from "../firebase";
-import {
-  getFirestore,
-  onSnapshot,
-  collection,
-  deleteDoc,
-  doc,
-  setDoc,
-} from "firebase/firestore";
 
 export default function LikeSection({ id }) {
   const { data: session } = useSession();
-  const [hasLiked, setHasLiked] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
   const [likes, setLikes] = useState([]);
   const db = getFirestore(app);
 
@@ -26,11 +26,8 @@ export default function LikeSection({ id }) {
   }, [db]);
 
   useEffect(() => {
-    console.log("likes changed");
     if (
-      likes.findIndex((like) => {
-        like.id === session?.user?.uid;
-      }) != -1
+      likes.findIndex((like) => { return like.id === session?.user?.uid}) !== -1
     ) {
       setHasLiked(true);
     } else {
@@ -39,33 +36,36 @@ export default function LikeSection({ id }) {
   }, [likes]);
 
   async function likePost() {
-    console.log("likePost");
-
+    console.log(db, id, 'likes', session?.user?.uid);
     if (hasLiked) {
-      console.log("if(hasLiked)");
-
-      await deleteDoc(doc(db, "posts", id, "likes", session?.user?.uid));
+      await deleteDoc(doc(db, 'posts', id, 'likes', session?.user?.uid));
     } else {
-      console.log("else");
-
-      await setDoc(doc(db, "posts", id, "likes", session?.user?.uid), {
+      await setDoc(doc(db, 'posts', id, 'likes', session?.user?.uid), {
         username: session?.user?.username,
       });
     }
   }
+
   return (
     <div>
       {session && (
         <div className="flex border-t border-gray-100 px-4 pt-4">
-          <div className="flex items-center">
+          <div className="flex items-center gap-2">
             {hasLiked ? (
               <HiHeart
                 className="text-red-500 cursor-pointer text-3xl hover:scale-125 transition-transform duration-200 ease-out"
                 onClick={likePost}
               />
             ) : (
-              <HiOutlineHeart className="cursor-pointer text-3xl hover:scale-125 transition-transform duration-200 ease-out" 
-              onClick={likePost}/>
+              <HiOutlineHeart
+                className="cursor-pointer text-3xl hover:scale-125 transition-transform duration-200 ease-out"
+                onClick={likePost}
+              />
+            )}
+            {likes.length > 0 && (
+              <p className="text-gray-500">
+                {likes.length} {likes.length > 1 ? "likes" : "like"}
+              </p>
             )}
           </div>
         </div>
