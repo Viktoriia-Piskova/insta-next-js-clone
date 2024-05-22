a9d70d939b1ce6baaaf33665220f17a5f5ee48a4
piskova <v.piskova@beetroot.se>
2024-05-22 18:55:56 +0300

    Display comments

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -4,15 +4,20 @@ import {
   addDoc,
   collection,
   getFirestore,
+  onSnapshot,
+  orderBy,
+  query,
   serverTimestamp,
 } from "firebase/firestore";
 import { useSession } from "next-auth/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { app } from "../firebase";
+import Moment from "react-moment";
 
 export default function CommentSection({ id }) {
   const { data: session } = useSession();
   const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
   const db = getFirestore(app);
 
   async function handleSubmit(e) {
@@ -28,8 +33,46 @@ export default function CommentSection({ id }) {
       timestamp: serverTimestamp(),
     });
   }
+
+  useEffect(() => {
+    onSnapshot(
+      query(
+        collection(db, "post", id, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => {
+        setComments(snapshot.docs);
+      }
+    );
+  }, [db]);
+
   return (
-    <div>
+    <div className="">
+      {comments.length > 0 && (
+        <div className="mx-10 overflow-y-scroll max-h-24">
+          {comments.map((comment, id) => (
+            <div
+              key={id}
+              className="flex items-center space-x-2 mb-2 justify-between"
+            >
+              <img
+                src={comment.data().userImage}
+                alt="user image"
+                className="h-7 rounded-full object-cover border p-[2px]"
+              />
+              <p className="text-sm flex-1 truncate">
+                <span className="font-bold text-gray-700">
+                  {comment.data().username}
+                </span>{" "}
+                {comment.data().comment}
+              </p>
+              <Moment fromNow className="text-xs text-gray-400 pr-2">
+                {comment.data().timestamp?.toDate()}
+              </Moment>
+            </div>
+          ))}
+        </div>
+      )}
       {session && (
         <form
           className="flex items-center p-4 gap-2"
