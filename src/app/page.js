1d70abe3752f4b8acd6eabe5e8c509301826a3a8
piskova <v.piskova@beetroot.se>
2024-05-23 17:43:41 +0300

    Add force dynamic for real time experience

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import React from "react";
-import Feed from '@/components/Feed'
+import Feed from "@/components/Feed";
+
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
